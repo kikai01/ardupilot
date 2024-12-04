a49319a5eb4ef68231a070aa6b110eee3db22c43
kikai01 <m201126@edu.asahikawa-nct.ac.jp>
2024-12-04 15:36:01 +0900

    Update version.h

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.1.5-test"
+#define THISFIRMWARE "ArduCopter V4.7.0-dev"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,7,0,FIRMWARE_VERSION_TYPE_DEV
